c96588f1a0f77c30cffe449aa2830506150fcdff
kaishvrya-pixel <kaishvrya@gmail.com>
2025-08-17 23:33:07 +0530

    Add files via upload

---
+++ history.js
@@ -0,0 +1,5 @@
+function historySkill(input) {
+  if (input.includes("independence")) return "India gained independence on 15 August 1947 🇮🇳.";
+  if (input.includes("world war 2")) return "World War II lasted from 1939 to 1945 🌍.";
+  return "I can tell you about Indian Independence or World War 2.";
+}
